1005e0cf4227485549ca2b5de1e523dcd82b05d4
Dan Ellis Echavarria <danellise@outlook.com>
2021-01-26 08:21:45 -0500

    Nav to javascript (eh)

--- js/index.js
+++ js/index.js
@@ -98,3 +98,16 @@ function removeAuthBehind()
   }
 
 }
+
+
+
+
+// Nav Behaviour
+let hamburger = document.getElementById('hamburgerbtn');
+
+let mobileMenu = document.getElementById('mobileMenu');
+
+hamburger.addEventListener('click', function(){
+    mobileMenu.classList.toggle("active");
+    console.log("owo");
+});
