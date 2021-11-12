4087561cf34bcada7e31a71ec40340019c3a7365
Dan Ellis Echavarria <19101das@gmail.com>
2021-11-11 19:18:54 -0500

    Update githubPages.js

--- js/githubPages.js
+++ js/githubPages.js
@@ -2,9 +2,5 @@ const PROJECT_TITLE = 'the-art-of-generality';
 const isGithub = false; // This is constant and should be modified in case of deployment to github pages
 export function u(url) {
     let str = url;
-    if(isGithub)
-    {
-        str = `/${PROJECT_TITLE}${url}`;
-    }
   return str;
 }
